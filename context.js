6038b43656547af67cb2dbe9f80b5e96e73de335
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-06-01 20:27:11 -1000

    Done

--- context.js
+++ context.js
@@ -17,14 +17,14 @@ var wall_e = new Robot("Wall-E");
 
 
 // #1
-console.log(  );
+console.log(add(0,1));
 
-// #2  
-console.log(  );
+// #2
+console.log(add.call(voltron, 1, 2));
 
 // #3
-console.log(  );
+console.log(add.apply(optimus,[20,30] ));
 
 // #4
-
-console.log(  );
+var bendIt=add.bind(null,'drinking',' beer')
+console.log( bendIt());
